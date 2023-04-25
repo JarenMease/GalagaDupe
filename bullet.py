ebbf78185a0d5a7b418eb9c20d88445a018c99e3
JarenMease <115202703+JarenMease@users.noreply.github.com>
2023-04-24 21:07:33 -0600

    Update bullet.py

--- bullet.py
+++ bullet.py
@@ -2,35 +2,21 @@ import pygame
 
 
 class Bullet(pygame.sprite.Sprite):
-    def __init__(self, x, y):
+    def __init__(self, bullet_image_path, x, y):
         super().__init__()
-        # self.image = pygame.Surface((2, 10))
-        # self.image = pygame.Surface((10, 20))
-        self.image = pygame.image.load("images/bullet_image.png").convert_alpha()
-        # self.image.fill((255, 255, 255))
+        self.image = pygame.image.load(bullet_image_path).convert_alpha()
         self.image = pygame.transform.scale(self.image, (10, 10))
         self.rect = self.image.get_rect()
-        # self.rect.x = x
-        # self.rect.y = y
         self.rect.centerx = x
         self.rect.bottom = y
-        # self.speed = 5
-        self.speed = 3
+        self.speed = 2
 
     def update(self):
-        # self.rect.y += self.speed
-        self.rect.move_ip(10, self.speed)
+        self.rect.move_ip(0, -self.speed)
+        if self.rect.bottom <= 0:
+            self.kill()
 
-    # def update(self):
-    #     self.rect.move_ip(0, self.speed)
-    #     if self.rect.bottom < 0:
-    #         self.kill()
-
-    # def update(self):
-    #     self.rect.y -= self.speed
-    #     if self.rect.bottom <= 0:
-    #         self.kill()
-
-    # def add(self, sprite_group):
-    #     sprite_group.add(self)
-        # sprite_group
+    def check_bullet_collision(self, enemy_group):
+        collisions = pygame.sprite.spritecollide(self, enemy_group, True)
+        if collisions:
+            self.kill()
