6342dd43653efcd043cc2ee192418a34ef4b0607
JarenMease <115202703+JarenMease@users.noreply.github.com>
2023-05-03 22:49:15 -0400

    Update bullet.py

--- bullet.py
+++ bullet.py
@@ -2,9 +2,9 @@ import pygame
 
 
 class Bullet(pygame.sprite.Sprite):
-    def __init__(self, bullet_image_path, x, y):
+    def __init__(self, image_path, x, y):
         super().__init__()
-        self.image = pygame.image.load(bullet_image_path).convert_alpha()
+        self.image = pygame.image.load(image_path).convert_alpha()
         self.image = pygame.transform.scale(self.image, (10, 10))
         self.rect = self.image.get_rect()
         self.rect.centerx = x
@@ -14,11 +14,14 @@ class Bullet(pygame.sprite.Sprite):
     def update(self):
         self.rect.move_ip(0, -self.speed)
         if self.rect.bottom <= 0:
-            self.kill()
+            self.reset()
 
-    def check_bullet_collision(self, enemy_group, points_score):
+    def check_bullet_collision(self, enemy_group, score):
         collisions = pygame.sprite.spritecollide(self, enemy_group, True)
         if collisions:
-            self.kill()
-            for alien in collisions:
-                points_score.add_points(alien.alien_points)
+            self.reset()
+            for enemy in collisions:
+                score.add_points(enemy.alien_points)
+
+    def reset(self):
+        self.kill()
